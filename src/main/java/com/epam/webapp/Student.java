61d7e34c6e0f611e3ea51a3c4a6fb19cb05ace94
lavanyamuvva1 <lavanyamuvva1@gmail.com>
2023-09-08 16:49:34 +0530

    added rds connection

--- src/main/java/com/epam/webapp/Student.java
+++ src/main/java/com/epam/webapp/Student.java
@@ -1,21 +1,21 @@
-//package com.epam.webapp;
-//
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.GeneratedValue;
-//import jakarta.persistence.GenerationType;
-//import jakarta.persistence.Id;
-//import lombok.Data;
-//
-//
-//
-//@Data
-//@Entity(name = "student")
-//public class Student{
-//   @Id
-//   @GeneratedValue(strategy = GenerationType.AUTO)
-//    private Integer id;
-//    private String name;
-//    private String email;
-//    private String mbl;
-//
-//}
+package com.epam.webapp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+
+
+@Data
+@Entity(name = "student")
+public class Student{
+   @Id
+   @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String name;
+    private String email;
+    private String mbl;
+
+}
--- src/main/java/com/epam/webapp/StudentController.java
+++ src/main/java/com/epam/webapp/StudentController.java
@@ -12,49 +12,45 @@ import java.util.List;
 @RequestMapping("/api/student")
 public class StudentController {
 
-    // Declare the service as final to ensure its immutability
-//    private final StudentService studentService;
-//
-//    // Use constructor-based dependency injection
-//    @Autowired
-//    public StudentController(StudentService studentService) {
-//        this.studentService = studentService;
-//    }
 
-//    @GetMapping
-//    public ResponseEntity<List<Student>> getAllStudents() {
-//        return ResponseEntity.ok(studentService.getAllStudents());
-//    }
-//
-//    @GetMapping("/{id}")
-//    public ResponseEntity<Student> getStudentById(@PathVariable String id) {
-//        return studentService.getStudentById(id)
-//                .map(ResponseEntity::ok)
-//                .orElse(ResponseEntity.notFound().build());
-//    }
-//
-//
-//    @PostMapping
-//    public ResponseEntity<Student> createStudent(@RequestBody Student student) {
-//
-//        Student student1=studentService.saveStudent(student);
-//        return ResponseEntity.ok(student1);
-//
-//    }
-//
-//    @PutMapping("/{id}")
-//    public ResponseEntity<Student> updateStudent(@PathVariable String id, @RequestBody Student student) {
-//        return ResponseEntity.ok(studentService.saveStudent(student));
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public ResponseEntity<Void> deleteStudent(@PathVariable String id) {
-//        studentService.deleteStudent(id);
-//        return ResponseEntity.noContent().build();
-//    }
-    @GetMapping
-    public String getMessage()
-    {
-        return "hello world ";
+    private final StudentService studentService;
+
+    // Use constructor-based dependency injection
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
     }
+
+    @GetMapping
+    public ResponseEntity<List<Student>> getAllStudents() {
+        return ResponseEntity.ok(studentService.getAllStudents());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Student> getStudentById(@PathVariable String id) {
+        return studentService.getStudentById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+
+    @PostMapping
+    public ResponseEntity<Student> createStudent(@RequestBody Student student) {
+
+        Student student1=studentService.saveStudent(student);
+        return ResponseEntity.ok(student1);
+
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Student> updateStudent(@PathVariable String id, @RequestBody Student student) {
+        return ResponseEntity.ok(studentService.saveStudent(student));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteStudent(@PathVariable String id) {
+        studentService.deleteStudent(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
